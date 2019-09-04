7fd8deeaf52b1d56bdc808d5c90cc193858e2eca
Mark Calvelo <combatdstar@gmail.com>
2019-09-04 15:09:23 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,7 @@
 //define functions here
 
+/*Define a function getIt that does not accept a parameter. The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".*/
+
 $(document).ready(function(){
 
 // call functions here

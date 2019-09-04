6dcdb78803a74314108284bb3bf7b04222f8b5ac
Mark Calvelo <combatdstar@gmail.com>
2019-09-04 15:57:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -16,7 +16,9 @@ function frameIt() {
 
 /*Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.*/
 function pressIt() {
-  $(document).on('keypress', )
+  $(document).on('keypress', () => {
+    
+  });
 }
 
 $(document).ready(function(){

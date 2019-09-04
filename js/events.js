6e1bbfdcaf33be43848f5b5fb3132c45dc4eeb23
Mark Calvelo <combatdstar@gmail.com>
2019-09-04 15:54:37 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,9 @@ function getIt() {
 
 /*Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.*/
 function frameIt() {
-  $('img')
+  $('img').on("load", () => {
+    $('img').a
+  });
 }
 
 $(document).ready(function(){
